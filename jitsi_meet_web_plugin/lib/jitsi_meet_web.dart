5796d6d8c2f46e36b00a7c0aadfcc412db0ff9d6
Nifal Nizar <nifal.nizar@hotmail.com>
2023-06-07 11:24:23 +0530

    jitsi_meet_web_plugin_local  version: 1.0.0

--- jitsi_meet_web_plugin/lib/jitsi_meet_web.dart
+++ jitsi_meet_web_plugin/lib/jitsi_meet_web.dart
@@ -5,7 +5,7 @@ import 'dart:ui' as ui;
 
 import 'package:flutter/material.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
-import 'package:jitsi_meet_platform_interface/jitsi_meet_platform_interface.dart';
+import 'package:jitsi_meet_platform_interface_local/jitsi_meet_platform_interface.dart';
 import 'package:js/js.dart';
 
 import 'jitsi_meet_external_api.dart' as jitsi;
